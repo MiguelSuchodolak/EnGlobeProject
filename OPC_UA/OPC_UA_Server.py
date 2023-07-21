a516c14aa27f04d1e6480a55d42e7f763fbfe78d
annazanotti <44625759+annazanotti@users.noreply.github.com>
2023-07-21 14:27:27 +0100

    Update OPC_UA_Server.py

--- OPC_UA/OPC_UA_Server.py
+++ OPC_UA/OPC_UA_Server.py
@@ -1,39 +1,77 @@
 import asyncio
 import logging
+import json
+from asyncua import Server
+from asyncio_mqtt import Client, MqttError
 
-from asyncua import Server, ua
-from asyncua.common.methods import uamethod
+import asyncio
+import asyncio_mqtt as aiomqtt
 
+# Setting local Broker IP address (= Raspberry Pi)
+LOCAL_BROKER = "192.168.131.219"
+# Default port for MQTT
+LOCAL_PORT = 1883
+
+# OPC UA server configuration
+OPC_SERVER_IP = "192.168.131.219"
+OPC_SERVER_PORT = 4840
+OPC_NAMESPACE = "EnGlobeNamespace"
+
+# Topic to subscribe from, data coming from the ESP32 + PLC
+topic_esp = "esp32/1"
+
+async def listen():
+    async with aiomqtt.Client(LOCAL_BROKER, LOCAL_PORT) as client:
+        async with client.messages() as messages:
+            await client.subscribe(topic_esp)
+            async for message in messages:
+                print(message.payload)
+                print(type(message.payload))
+                print(str(message.payload))
+                msg_json = json.loads(message.payload.decode("utf-8","ignore"))
+
+                await temp.write_value(msg_json["FurnaceTemperature"])
+                await pressure.write_value(msg_json["FurnacePressure"])
+                await time_stamp.write_value(msg_json["Timestamp"])
+
+async def handle():
+    # Wait for messages in (unawaited) asyncio task
+    loop = asyncio.get_event_loop()
+    task = loop.create_task(listen())
+    await task
 
 async def main():
+    global temp
+    global pressure
+    global time_stamp
     _logger = logging.getLogger(__name__)
     # setup our server
     server = Server()
     await server.init()
-    server.set_endpoint("opc.tcp://localhost:4840/freeopcua/server/")
+    server.set_endpoint(f"opc.tcp://{OPC_SERVER_IP}:{OPC_SERVER_PORT}")
 
-    # set up our own namespace, not really necessary but should as spec
-    uri = "EnGlobeNamespace"
-    idx = await server.register_namespace(uri)
-
-    #Espera até receber o ESP
+    idx = await server.register_namespace(OPC_NAMESPACE)
 
     # populating our address space
-    # server.nodes, contains links to very common nodes like objects and root
     obj = await server.nodes.objects.add_object(idx, "EnGlobe")
-    temp = await obj.add_variable(idx, "Temperature", 6.7)
+    temp = await obj.add_variable(idx, "Temperature", 50.0)
+    pressure = await obj.add_variable(idx, "Pressure", 0.0)
+    time_stamp = await obj.add_variable(idx, "Timestamp", "")
+
     # Set MyVariable to be writable by clients
     await temp.set_writable()
+    await pressure.set_writable()
+    await time_stamp.set_writable()
+
     _logger.info("Starting server!")
-    
+
     async with server:
         while True:
             await asyncio.sleep(1)
-            new_val = await temp.get_value() + 0.1
-            _logger.info("Set value of %s to %.1f", temp, new_val)
-            await temp.write_value(new_val)
 
+async def main_coroutine():
+    await asyncio.gather(handle(), main())
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
-    asyncio.run(main(), debug=True)
+    asyncio.run(main_coroutine())
